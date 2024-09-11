b4316fd68935dc36e639159eb4865d3a3831525f
Davilson <53828575+Davilson26@users.noreply.github.com>
2024-09-11 12:35:02 -0500

    Update index.vue

--- resources/js/src/views/devices/index.vue
+++ resources/js/src/views/devices/index.vue
@@ -83,7 +83,9 @@
                 nextArrow='<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
             >
             <template #engine_status="data">
-                <span class="badge" :class="data.device_data.engine_status ? 'badge-outline-success':'badge-outline-danger'">{{data.device_data.engine_status ? 'Encendido':'Apagado' }}</span>
+                <div v-if="data.value?.device_data">
+                    <span class="badge" :class="data.value.device_data.engine_status ? 'badge-outline-success':'badge-outline-danger'">{{data.value.device_data.engine_status ? 'Encendido':'Apagado' }}</span>
+                </div>
             </template>
         </vue3-datatable>
         </div>
@@ -193,7 +195,7 @@
     // channel.bind('ejetrack.webhook.device', function() {
     //     console.log('Evento device recibido');
     // });
-    
+
     onMounted(async () => {
        await getDevicesData();
     });
