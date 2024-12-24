a4afc88f61f7fa489ee023a0f10656203f415aaa
Davilson <53828575+Davilson26@users.noreply.github.com>
2024-12-24 10:36:13 -0500

    Update signature.vue

--- resources/js/src/views/singit/contracts/signature.vue
+++ resources/js/src/views/singit/contracts/signature.vue
@@ -87,7 +87,7 @@
                                         </svg>
                                     </template>
                                     <template
-                                        v-if="attachment.type !== 'zip' && attachment.type !== 'image' && attachment.type !== 'folder'">
+                                        v-if="attachment.type !== 'zip' && attachment.type !== 'image/jpeg' && attachment.type !== 'folder'">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                             xmlns="http://www.w3.org/2000/svg" class="w-5 h-5">
                                             <path
@@ -755,6 +755,7 @@ const acceptPhotoAndNextStep = async () => {
             }
         } catch (error) {
             console.error("Error al enviar la foto:", error);
+            uploadPhoto.value = false;
             notify.showToast("Hubo un error al enviar la foto. Por favor, intenta de nuevo.", "error");
         }
     }
